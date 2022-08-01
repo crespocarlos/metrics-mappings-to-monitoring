21b57e7b4e215dbc88d7516a19e06bd37740f3db
klacabane <kevin.lacabane@elastic.co>
2022-08-01 12:17:00 +0200

    tls

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 const axios = require("axios");
 const { merge } = require("lodash");
 
@@ -6,26 +8,26 @@ const auth = { username: "elastic", password: "changeme" };
 
 module.exports.installIntegrationAssets = (name, version) =>
   axios.post(
-    `http://localhost:5601${kibanaBasepath}/api/fleet/epm/packages/${name}/${version}`,
+    `https://localhost:5601${kibanaBasepath}/api/fleet/epm/packages/${name}/${version}`,
     { force: true },
     { auth, headers: { "kbn-xsrf": "1" } }
   );
 
 module.exports.createIndexTemplate = (name, payload) =>
-  axios.put(`http://localhost:9200/_index_template/${name}`, payload, { auth });
+  axios.put(`https://localhost:9200/_index_template/${name}`, payload, { auth });
 
 module.exports.deleteDS = (pattern) =>
-  axios.delete(`http://localhost:9200/_data_stream/${pattern}`, { auth });
+  axios.delete(`https://localhost:9200/_data_stream/${pattern}`, { auth });
 
 module.exports.deleteTemplate = (name) =>
-  axios.delete(`http://localhost:9200/_index_template/${name}`, { auth });
+  axios.delete(`https://localhost:9200/_index_template/${name}`, { auth });
 
 const getComponentTemplate = (name) =>
-  axios.get(`http://localhost:9200/_component_template/${name}`, { auth });
+  axios.get(`https://localhost:9200/_component_template/${name}`, { auth });
 
 module.exports.getIndexTemplates = async (templateName) => {
   const { data } = await axios.get(
-    `http://localhost:9200/_index_template/${templateName}`,
+    `https://localhost:9200/_index_template/${templateName}`,
     { auth }
   );
   const { index_templates: indexTemplates } = data;
